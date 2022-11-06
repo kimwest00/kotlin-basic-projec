b20c51e3b1e197a75fd9ca839ddc27bd9edb4f04
kimwest00 <kimwest00@hufs.ac.kr>
2022-11-06 13:18:24 +0900

    FEAT - memo activity editText 데이터 넘기기

--- week5/app/src/main/java/umc/standard/week5/MemoActivity.kt
+++ week5/app/src/main/java/umc/standard/week5/MemoActivity.kt
@@ -1,6 +1,7 @@
 package umc.standard.week5
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import umc.standard.week5.databinding.ActivityMainBinding
@@ -12,7 +13,12 @@ class MemoActivity() : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMemoBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-
+        var memo_text = viewBinding.etMemoWrite.text
+        viewBinding.btMemoSave.setOnClickListener{
+            var intent = Intent(this,MainActivity::class.java)
+            intent.putExtra("memo text","${memo_text}")
+            startActivity(intent)
+        }
 
     }
 }
