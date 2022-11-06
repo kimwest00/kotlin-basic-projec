2f4e81e2728adeb99fd368ae9d1d1425f94f39b7
kimwest00 <kimwest00@hufs.ac.kr>
2022-11-06 11:55:13 +0900

    FIX - item xml 수정; 스크롤시 간격

--- week5/app/src/main/java/umc/standard/week5/MainActivity.kt
+++ week5/app/src/main/java/umc/standard/week5/MainActivity.kt
@@ -10,18 +10,34 @@ import umc.standard.week5.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewBinding : ActivityMainBinding
-    var dataList= ArrayList<BusinessCard>()
+    var dataList = ArrayList<BusinessCard>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        for(x in 0..30){
-            dataList.add(BusinessCard(name = "록", content ="하이"))
-            dataList.add(BusinessCard(name = "록", content ="하이"))
-            dataList.add(BusinessCard(name = "록", content ="하이"))
-            dataList.add(BusinessCard(name = "록", content ="하이"))
-        }
+        dataList.apply {
+            add(BusinessCard("김민서","안녕하세요"))
+            add(BusinessCard("김만서","hello"))
+            add(BusinessCard("김민수","hola"))
+            add(BusinessCard("김만수","곤니치와"))
+            add(BusinessCard("김민서","안녕하세요"))
+            add(BusinessCard("김만서","hello"))
+            add(BusinessCard("김민수","hola"))
+            add(BusinessCard("김만수","곤니치와"))
+            add(BusinessCard("김민서","안녕하세요"))
+            add(BusinessCard("김만서","hello"))
+            add(BusinessCard("김민수","hola"))
+            add(BusinessCard("김만수","곤니치와"))
+            add(BusinessCard("김민서","안녕하세요"))
+            add(BusinessCard("김만서","hello"))
+            add(BusinessCard("김민수","hola"))
+            add(BusinessCard("김만수","곤니치와"))
+            add(BusinessCard("김민서","안녕하세요"))
+            add(BusinessCard("김만서","hello"))
+            add(BusinessCard("김민수","hola"))
+            add(BusinessCard("김만수","곤니치와"))
+         }
         Log.d("dataList 출력","start")
         print(dataList)
         val dataRVAdapter = DataRVAdapter(dataList)
