bc3b97d862b744c8ead1ce7fb07bdaae45882d10
kimwest00 <kimwest00@hufs.ac.kr>
2022-11-13 17:55:25 +0900

    FIX - viewbinding 설정 코드 누락

--- week6/app/src/main/java/umc/standard/myapplication/MainActivity.kt
+++ week6/app/src/main/java/umc/standard/myapplication/MainActivity.kt
@@ -9,7 +9,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportFragmentManager
             .beginTransaction()
             .replace(binding.frameLayout.id, HomeFragment())
