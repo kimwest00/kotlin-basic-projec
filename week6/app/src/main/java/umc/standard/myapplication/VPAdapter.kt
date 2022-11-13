c042682d9f68b567fcdd4c3e5f0e35219195c6e3
kimwest00 <kimwest00@hufs.ac.kr>
2022-11-13 18:26:56 +0900

    FIX -과제 요구사항맞춰 탭 3개로

--- week6/app/src/main/java/umc/standard/myapplication/VPAdapter.kt
+++ week6/app/src/main/java/umc/standard/myapplication/VPAdapter.kt
@@ -5,13 +5,14 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class ViewPagerAdapter(fragmentActivity: HomeFragment): FragmentStateAdapter(fragmentActivity) {
     // 페이지 갯수 설정
-    override fun getItemCount() = 2
+    override fun getItemCount() = 3
 
     // 불러올 Fragment 정의
     override fun createFragment(position: Int): Fragment{
         return when (position){
             0 -> CameraFragment()
             1 -> PlantFragment()
+            2 -> HomeFragment()
             else -> HomeFragment()
         }
 
