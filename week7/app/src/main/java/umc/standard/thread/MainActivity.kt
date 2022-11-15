886f1587d40853af99f5d7517f341ef34d2a9777
kimwest00 <kimwest00@hufs.ac.kr>
2022-11-15 17:28:56 +0900

    FIX - button 추가로 리스너에 등록

--- week7/app/src/main/java/umc/standard/thread/MainActivity.kt
+++ week7/app/src/main/java/umc/standard/thread/MainActivity.kt
@@ -5,26 +5,28 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import umc.standard.thread.databinding.ActivityMainBinding
-import java.lang.Thread.sleep
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    var handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Thread(){
-
-            for(i in 30 until 0){
-                Thread.sleep(1000)
-                handler.post{
-                    binding.tvTimerLimit.setText(i)
+        var handler = Handler(Looper.getMainLooper())
+        binding.btTimerStart.setOnClickListener {
+            Thread(){
+                for(i in 30 until 0){
+                    Thread.sleep(1000)
+                    handler.post{
+                        binding.tvTimerLimit.setText(i)
+                        println("timer working at :")
+                        println(i)
+                    }
                 }
-            }
-
-            binding.tvTimerLimit.setText("finish!")
-
-        }.start()
+                binding.tvTimerLimit.setText("finish!")
+            }.start()
+        }
     }
 }
