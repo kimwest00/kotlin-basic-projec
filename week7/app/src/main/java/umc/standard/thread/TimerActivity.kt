90531e817216502aea8ab7b2973195aa84de067c
kimwest00 <kimwest00@hufs.ac.kr>
2022-11-16 23:26:40 +0900

    FIX - cast 관련 오류 해결//standard 미션완료

--- week7/app/src/main/java/umc/standard/thread/TimerActivity.kt
+++ week7/app/src/main/java/umc/standard/thread/TimerActivity.kt
@@ -3,7 +3,6 @@ package umc.standard.thread
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import umc.standard.thread.databinding.ActivityMainBinding
 import umc.standard.thread.databinding.ActivityTimerBinding
 import kotlin.concurrent.thread
 
@@ -15,20 +14,27 @@ class TimerActivity:AppCompatActivity() {
         binding = ActivityTimerBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val extra = intent.extras
-        var time = extra?.get("time")
+        var time = extra?.get("time").toString().toInt()
         Log.d("time limit",time.toString())
         binding.tvTimerNum.text = time.toString()
-//        thread(start = true){
-//            for(i in 30 until 0){
-//                Thread.sleep(1000)
-//                runOnUiThread{
-//                    time_text.text = time.toString()
-//                }
-//                println("timer working at :")
-//                println(i)
-//            }
-////                text.text= "finish!"
-//        }
+        thread(start = true){
+            while(time > 0){
+                Thread.sleep(1000)
+                time -= 1
+                runOnUiThread{
+                    binding.tvTimerNum.text = time.toString()
+                }
+            }
+            runOnUiThread{
+                binding.tvTimerNum.text = "Time is over!"
+            }
+        }
     }
 
     }
+
+
+
+
+
+
